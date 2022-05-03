dcd9c5eb3258a58c2a59f09603686b728f7b5bf7
GoldenMine0502 <ehe123@naver.com>
2022-05-03 14:44:20 +0900

    Initial commit

---
+++ src/test/java/kr/goldenmine/flowerserver/FlowerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.goldenmine.flowerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
